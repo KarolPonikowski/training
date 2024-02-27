d61acb9be5d6c9891535a300f84c91e96a76326a
Karol Ponikowski <k.ponikowski.98@gmail.com>
2024-02-27 12:13:48 +0100

    Delete usless code from main,dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:training/firebase_options.dart';
 
@@ -10,10 +9,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  FirebaseUIAuth.configureProviders([
-    EmailAuthProvider(),
-    // ... other providers
-  ]);
+
   runApp(const MyApp());
 }
 
