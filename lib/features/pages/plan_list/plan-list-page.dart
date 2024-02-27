57494cfedb8e2f5148fefac6b3b5d6aaab00a029
Karol Ponikowski <k.ponikowski.98@gmail.com>
2024-02-27 14:21:56 +0100

    Add plan list page

---
+++ lib/features/pages/plan_list/plan-list-page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlanListPage extends StatefulWidget {
+  const PlanListPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<PlanListPage> createState() => _PlanListPageState();
+}
+
+class _PlanListPageState extends State<PlanListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
