0f27e4e9750828c3504a199e9b8be3d9400c2347
Karol Ponikowski <k.ponikowski.98@gmail.com>
2024-03-16 11:33:12 +0100

    Add Function updatePlanId

--- lib/models/plan_exercise_model.dart
+++ lib/models/plan_exercise_model.dart
@@ -1,15 +1,13 @@
 import 'dart:ffi';
 
-import 'training_plan_model.dart';
-
 class PlanExerciseModel {
   PlanExerciseModel({
     required this.id,
-    required TraninigPlanModel plan,
+    required this.planId,
     required this.exerciseId,
     required this.weight,
     required this.reps,
-  }) : planId = plan.id;
+  });
 
   final String id;
   final String planId;
